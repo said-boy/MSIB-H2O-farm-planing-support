4b5955e64f08abd7730e0eca1b59e178c09b4e0a
said-boy <alkhudrimsaid733@gmail.com>
2024-09-25 15:09:16 +0700

    halaman dashboard sudah selesai

--- frontend/src/component/dashbord/ContactUs.jsx
+++ frontend/src/component/dashbord/ContactUs.jsx
@@ -5,7 +5,7 @@ export default function ContactUs() {
         <>
             <div>
                 <div className="md:flex md:flex-col">
-                    <h1 className="font-bold text-4xl pb-4 pt-8 mb-5 font-poppins font-semibold lg:text-center">Kami menunggu kehadiran anda.</h1>
+                    <h1 className="font-bold text-4xl pb-4 pt-8 mb-5 font-poppins font-semibold md:text-center">Kami menunggu kehadiran anda.</h1>
                     <p className="text-center">Bergabunglah dengan kami dan jadilah bagian dari misi penghijauan dunia. setiap langkah kecil Anda membawa perubahan besar!</p>
                     <Button color="success" className="text-white mt-12 p-7 px-20 font-bold text-lg w-full md:w-1/3 md:self-center drop-shadow-[0_4px_12px_rgba(255,221,40,0.2)]">
                         Let's go farm
